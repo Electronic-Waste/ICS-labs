f0e840934342597f0d4b8665fdd12f1c58c53f02
Electronic-Waste <2690692950@qq.com>
2022-05-06 09:13:03 -0400

    lab8 finished with score: 53

--- lab8/trans.c
+++ lab8/trans.c
@@ -24,12 +24,162 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
+    int i ,j , k;
+    int tmp1, tmp2, tmp3, tmp4, tmp5, tmp6, tmp7, tmp8;
+
+    if (M == 32 && N == 32) {
+        for (i = 0; i < N; i += 8) {
+            for (j = 0; j < M; j += 8) {
+                for (int k = i; k < i + 8; ++k) {
+                    tmp1 = A[k][j];
+                    tmp2 = A[k][j + 1];
+                    tmp3 = A[k][j + 2];
+                    tmp4 = A[k][j + 3];
+                    tmp5 = A[k][j + 4];
+                    tmp6 = A[k][j + 5];
+                    tmp7 = A[k][j + 6];
+                    tmp8 = A[k][j + 7];
+
+                    B[j][k] = tmp1;
+                    B[j + 1][k] = tmp2;
+                    B[j + 2][k] = tmp3;
+                    B[j + 3][k] = tmp4;
+                    B[j + 4][k] = tmp5;
+                    B[j + 5][k] = tmp6;
+                    B[j + 6][k] = tmp7;
+                    B[j + 7][k] = tmp8;
+                } 
+            }
+        }
+    }
+    else if (M == 64 && N == 64) {
+        for (i = 0; i < N; i += 8) {
+            for (j = 0; j < M; j += 8) {
+                for (k = i; k < i + 4; ++k) {
+                    tmp1 = A[k][j];
+                    tmp2 = A[k][j + 1];
+                    tmp3 = A[k][j + 2];
+                    tmp4 = A[k][j + 3];
+                    tmp5 = A[k][j + 4];
+                    tmp6 = A[k][j + 5];
+                    tmp7 = A[k][j + 6];
+                    tmp8 = A[k][j + 7];
+
+                    B[j][k] = tmp1;
+                    B[j + 1][k] = tmp2;
+                    B[j + 2][k] = tmp3;
+                    B[j + 3][k] = tmp4;
+                    B[j][k + 4] = tmp5;
+                    B[j + 1][k + 4] = tmp6;
+                    B[j + 2][k + 4] = tmp7;
+                    B[j + 3][k + 4] = tmp8;
+
+                }
+                for (k = j; k < j+4; k++)
+                {
+                    tmp1 = A[i + 4][k];
+                    tmp2 = A[i + 5][k];
+                    tmp3 = A[i + 6][k];
+                    tmp4 = A[i + 7][k];
+
+                    tmp5 = B[k][i + 4];
+                    tmp6 = B[k][i + 5];
+                    tmp7 = B[k][i + 6];
+                    tmp8 = B[k][i + 7];
+
+                    B[k][i + 4] = tmp1;
+                    B[k][i + 5] = tmp2;
+                    B[k][i + 6] = tmp3;
+                    B[k][i + 7] = tmp4;
+
+                    B[k + 4][i] = tmp5;
+                    B[k + 4][i + 1] = tmp6;
+                    B[k + 4][i + 2] = tmp7;
+                    B[k + 4][i + 3] = tmp8;
+                }
+
+                for (int k = i + 4; k < i + 8; ++k) {
+                    tmp1 = A[k][j + 4];
+                    tmp2 = A[k][j + 5];
+                    tmp3 = A[k][j + 6];
+                    tmp4 = A[k][j + 7];
+
+                    B[j + 4][k] = tmp1;
+                    B[j + 5][k] = tmp2;
+                    B[j + 6][k] = tmp3;
+                    B[j + 7][k] = tmp4;
+                }
+            }
+        }
+    }
+    else if (M == 61 && N == 67) {
+        for (i = 0; i < 64; i += 8) {
+            for (j = 0; j < 56; j += 8) {
+                for (k = i; k < i + 8; ++k) {
+                    tmp1 = A[k][j];
+                    tmp2 = A[k][j + 1];
+                    tmp3 = A[k][j + 2];
+                    tmp4 = A[k][j + 3];
+                    tmp5 = A[k][j + 4];
+                    tmp6 = A[k][j + 5];
+                    tmp7 = A[k][j + 6];
+                    tmp8 = A[k][j + 7];
+
+                    B[j][k] = tmp1;
+                    B[j + 1][k] = tmp2;
+                    B[j + 2][k] = tmp3;
+                    B[j + 3][k] = tmp4;
+                    B[j + 4][k] = tmp5;
+                    B[j + 5][k] = tmp6;
+                    B[j + 6][k] = tmp7;
+                    B[j + 7][k] = tmp8;
+                } 
+            }
+        }
+        for (j = 0; j < 56; j += 8) {
+            for (i = 64; i < N; ++i) {
+                tmp1 = A[i][j];
+                tmp2 = A[i][j + 1];
+                tmp3 = A[i][j + 2];
+                tmp4 = A[i][j + 3];
+                tmp5 = A[i][j + 4];
+                tmp6 = A[i][j + 5];
+                tmp7 = A[i][j + 6];
+                tmp8 = A[i][j + 7];
+
+                B[j][i] = tmp1;
+                B[j + 1][i] = tmp2;
+                B[j + 2][i] = tmp3;
+                B[j + 3][i] = tmp4;
+                B[j + 4][i] = tmp5;
+                B[j + 5][i] = tmp6;
+                B[j + 6][i] = tmp7;
+                B[j + 7][i] = tmp8;
+            }
+        }
+        for (i = 0; i < N; i += 1) {
+                tmp1 = A[i][56];
+                tmp2 = A[i][57];
+                tmp3 = A[i][58];
+                tmp4 = A[i][59];
+                tmp5 = A[i][60];
+
+                B[56][i] = tmp1;
+                B[57][i] = tmp2;
+                B[58][i] = tmp3;
+                B[59][i] = tmp4;
+                B[60][i] = tmp5;
+        }
+
+    }
+     
 }
 
 /* 
  * You can define additional transpose functions below. We've defined
  * a simple one below to help you get started. 
  */ 
+
 
 /* 
  * trans - A simple baseline transpose function, not optimized for the cache.
@@ -44,8 +194,7 @@ void trans(int M, int N, int A[N][M], int B[M][N])
             tmp = A[i][j];
             B[j][i] = tmp;
         }
-    }    
-
+    }
 }
 
 /*
