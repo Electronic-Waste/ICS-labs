5ab72e23a2bd580f81438d29923a857666a594be
Electronic-Waste <2690692950@qq.com>
2022-05-05 23:14:49 -0400

    PartA Finished

--- lab8/csim.c
+++ lab8/csim.c
@@ -19,7 +19,7 @@ int block_bits;                 // Block offset
 typedef struct line
 {
     int valid;
-    int tag;
+    long tag;
     int block;
     int size;
 } line_t;
@@ -35,7 +35,6 @@ set_t *cache;
 void build_cache()
 {
     int set_num = 2 << index_bits;
-    int ass_num = ass_num;
 
     cache = (set_t *) malloc(sizeof(set_t) * set_num);
     for (int i = 0; i < set_num; ++i) {
@@ -52,7 +51,7 @@ void build_cache()
    
 }
 
-int getTag(long addr)
+long getTag(long addr)
 {
     return (addr >> (index_bits + block_bits));
 }
@@ -73,20 +72,30 @@ int getBlock(long addr)
     return addr & mask;
 }
 
+void display()
+{
+    int set_num = 1 << index_bits;
+    for (int i = 0; i < set_num; ++i)
+        for (int j = 0; j < ass_num; ++j) {
+            line_t line = cache[i].lineGrp[j];
+            printf("set:%d line:%d valid:%d tag:%ld\n", i, j, line.valid, line.tag);
+        }
+}
+
 void load(long addr, int size)
 {
-    int tag = getTag(addr);
+    long tag = getTag(addr);
     int set_index = getSet(addr);
     int block_offset = getBlock(addr);
 
     int index = 0;
+    int empty_index = 0;
     bool isHit = false;
     bool hasEmptyLine = false;
     for (int i = 0; i < ass_num; ++i) {
         line_t line = cache[set_index].lineGrp[i];
         // printf("line valid: %d tag:%d block:%d\n", line.valid, line.tag, line.block);
         index = (cache[set_index].used[i] >= cache[set_index].used[index]) ? i : index; //LRU index
-        if (line.valid == 0) hasEmptyLine = true;       //check if exists empty line
         
         /* memory hit */
         if (line.valid == 1 &&  line.tag == tag) {
@@ -95,23 +104,32 @@ void load(long addr, int size)
             cache[set_index].used[i] = 0;               //if accessed, used[i] set to 0
             if (verbose) {
                 printf("L %lx,%d hit\n", addr, size);
+                printf("set: %d ass: %d\n", set_index, i);
             }
         }
         /* not hit */
-        else {
+        else if (line.valid == 1) {
             cache[set_index].used[i]++;                 //else ++used[i]
+        }
+        /* has empty line or not */
+        else {
+            hasEmptyLine = true;
+            empty_index = i;
         }
     }
 
-    if (isHit) return;
+    if (isHit) ;
     else if (hasEmptyLine) {
-        cache[set_index].lineGrp[index].valid = 1;
-        cache[set_index].lineGrp[index].tag = tag;
-        cache[set_index].lineGrp[index].block = block_offset;
-        cache[set_index].lineGrp[index].size = size;
-        cache[set_index].used[index] = 0;
+        cache[set_index].lineGrp[empty_index].valid = 1;
+        cache[set_index].lineGrp[empty_index].tag = tag;
+        cache[set_index].lineGrp[empty_index].block = block_offset;
+        cache[set_index].lineGrp[empty_index].size = size;
+        cache[set_index].used[empty_index] = 0;
         ++miss_count;
-        if (verbose) printf("L %lx,%d miss\n", addr, size);
+        if (verbose) {
+            printf("L %lx,%d miss\n", addr, size);
+            printf("set: %d ass: %d\n", set_index, empty_index);
+        }
     }
     else {
         cache[set_index].lineGrp[index].valid = 1;
@@ -121,26 +139,31 @@ void load(long addr, int size)
         cache[set_index].used[index] = 0;
         ++miss_count;
         ++eviction_count;
-        if (verbose) printf("L %lx,%d miss eviction\n", addr, size);
+        if (verbose) {
+            printf("L %lx,%d miss eviction\n", addr, size);
+            printf("set: %d ass: %d\n", set_index, index);
+        }
     }
     //printf("eviction: %d\n", eviction_count);
+
+    if (verbose) display();
 
 }
 
 void store(long addr, int size)
 {
-    int tag = getTag(addr);
+    long tag = getTag(addr);
     int set_index = getSet(addr);
     int block_offset = getBlock(addr);
 
     int index = 0;
+    int empty_index = 0;
     bool isHit = false;
     bool hasEmptyLine = false;
     for (int i = 0; i < ass_num; ++i) {
         line_t line = cache[set_index].lineGrp[i];
         //printf("line valid: %d tag:%d block:%d\n", line.valid, line.tag, line.block);
         index = (cache[set_index].used[i] >= cache[set_index].used[index]) ? i : index; //LRU index
-        if (line.valid == 0) hasEmptyLine = true;       //check if exists empty line
         
         /* memory hit */
         if (line.valid == 1 &&  line.tag == tag) {
@@ -151,23 +174,32 @@ void store(long addr, int size)
             cache[set_index].lineGrp[i].size = size;            //update size
             if (verbose) {
                 printf("S %lx,%d hit\n", addr, size);
+                printf("set: %d ass: %d\n", set_index, i);
             }
         }
         /* not hit */
-        else {
+        else if (line.valid == 1){
             cache[set_index].used[i]++;                 //else ++used[i]
+        }
+        /* has empty line or not */
+        else {
+            hasEmptyLine = true;
+            empty_index = i;
         }
     }
 
-    if (isHit) return;
+    if (isHit) ;
     else if (hasEmptyLine) {
-        cache[set_index].lineGrp[index].valid = 1;
-        cache[set_index].lineGrp[index].tag = tag;
-        cache[set_index].lineGrp[index].block = block_offset;
-        cache[set_index].lineGrp[index].size = size;
-        cache[set_index].used[index] = 0;
+        cache[set_index].lineGrp[empty_index].valid = 1;
+        cache[set_index].lineGrp[empty_index].tag = tag;
+        cache[set_index].lineGrp[empty_index].block = block_offset;
+        cache[set_index].lineGrp[empty_index].size = size;
+        cache[set_index].used[empty_index] = 0;
         ++miss_count;
-        if (verbose) printf("S %lx,%d miss\n", addr, size);
+        if (verbose) {
+            printf("S %lx,%d miss\n", addr, size);
+            printf("set: %d ass: %d\n", set_index, empty_index);
+        }
     }
     else {
         cache[set_index].lineGrp[index].valid = 1;
@@ -177,9 +209,13 @@ void store(long addr, int size)
         cache[set_index].used[index] = 0;
         ++miss_count;
         ++eviction_count;
-        if (verbose) printf("S %lx,%d miss eviction\n", addr, size);
+        if (verbose) {
+            printf("S %lx,%d miss eviction\n", addr, size);
+            printf("set: %d ass: %d\n", set_index, index);
+        }
     }
     //printf("eviction: %d\n", eviction_count);
+    if (verbose) display();
 }
 
 void modify(long addr, int size)
