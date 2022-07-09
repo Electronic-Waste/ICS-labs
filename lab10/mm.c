053e342335835e7ae7182e322720c5536791ae3c
Electronic-Waste <2690692950@qq.com>
2022-07-09 02:46:41 -0400

    Upload lab9,lab10 from elsewhere

--- lab10/mm.c
+++ lab10/mm.c
@@ -1,13 +1,14 @@
 /*
- * mm-naive.c - The fastest, least memory-efficient malloc package.
+ * Name: Wang Shao
+ * ID: 520021911427
+ * Descitpion: I take the segregated fits strategy by creating 8 free lists, storing their head in the first
+ * 64 bytes of the heap(each has 8 bytes since address is encoded with 8 bytes in x86-64 machine). In my implementation, 
+ * each block has its own header and footer. What's more, every free block has SUCC and PRED pointer, which point
+ * to their next or previous free blocks in certain free lists (recall that we have 8 free lists). Taking those stategies,
+ * I designed some helper functions such as get_index, insert_node, remove node to maintain segregated free lists and some
+ * checker functions, for example, seg_header_checker, print_freeList and print_heap. 
  * 
- * In this naive approach, a block is allocated by simply incrementing
- * the brk pointer.  A block is pure payload. There are no headers or
- * footers.  Blocks are never coalesced or reused. Realloc is
- * implemented directly using mm_malloc and mm_free.
- *
- * NOTE TO STUDENTS: Replace this header comment with your own header
- * comment that gives a high level description of your solution.
+ * The following code shows my thoughts:
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -27,12 +28,165 @@
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
+/* Basic constants and marcos */
+#define WSIZE 4
+#define DSIZE 8
+#define CHUNKSIZE (1 << 12)
+#define FREELIST_NUM 8
+
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+
+/* Pack  a size and allocated bit into a word */
+#define PACK(size, alloc) ((size) | (alloc))
+
+/* Read and write a word at address p */
+#define GET(p)      (*(unsigned int *) (p))
+#define PUT(p, val) (*(unsigned int *) (p) = (val))
+#define PUT_LONG(p, val)    (*(unsigned long *) (p) = (val))
+
+/* Read the size and allocated fields from address p */
+#define GET_SIZE(p) (GET(p) & ~0x7)
+#define GET_ALLOC(p)(GET(p) & 0x1)
+
+/* Given block ptr bp, compute address of its header and footer */
+#define HDRP(bp)    ((char *)(bp) - WSIZE)
+#define FTRP(bp)    ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
+
+/* Given block ptr bp, compute address of next and previous blocks */
+#define NEXT_BLKP(bp)   ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE)))
+#define PREV_BLKP(bp)   ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)))
+
+/* Given block ptr bp, compute address of pred and succ nodes in free list */
+#define PRED(bp)        ((char *)(bp))
+#define SUCC(bp)        ((char *)(bp) + DSIZE)
+#define GET_PRED(bp)    (*(unsigned long *) (PRED(bp)))
+#define GET_SUCC(bp)    (*(unsigned long *) (SUCC(bp)))
+
+/* Debug Mode */
+#define DEBUGx
+
+char *seg_freelist_head;        
+
+char *heap_listp;               
+
+void seg_header_checker();      // checker for segregated free lists head
+
+void print_freeList();          // checker for the content in each free list
+
+void print_heap();              // checker for the content in the heap
+
+size_t get_index(size_t size);
+
+static void insert_node(void *bp);
+
+static void remove_node(void *bp);
+
+static void *extend_heap(size_t words);
+
+static void *coalesce(void *bp);
+
+static void *find_fit(size_t asize);
+
+static void place(void *bp, size_t asize);
+
+/**
+ * @brief Get the index of free list in seg-free-list
+ * @param size size of the block
+ * @return size_t the index of free list
+ */
+size_t get_index(size_t size)
+{
+    int index;
+
+    for (index = 11; index > 4; --index) {
+        if ((size >> index) > 0) {
+            break;
+        }
+    }
+    return (index - 4);
+}
+
+/**
+ * @brief insert block into free list
+ * @param bp pointer to payload of the block
+ */
+static void insert_node(void *bp)
+{
+    size_t size = GET_SIZE(HDRP(bp));
+    size_t index = get_index(size);
+    char *start = *(unsigned long *) (seg_freelist_head + index * DSIZE);
+    
+    if (start == NULL) {    //the free list is empty
+        PUT_LONG(seg_freelist_head + index * DSIZE, bp);
+        PUT_LONG(PRED(bp), NULL);
+        PUT_LONG(SUCC(bp), NULL);
+    }
+    else {                  //otherwise      
+        PUT_LONG(seg_freelist_head + index * DSIZE, bp);
+        PUT_LONG(PRED(bp), NULL);
+        PUT_LONG(SUCC(bp), start);
+        PUT_LONG(PRED(start), bp);
+    }
+}
+
+/**
+ * @brief remove block from free list
+ * @param bp pointer to payload of the block
+ */
+static void remove_node(void *bp)
+{
+    size_t size = GET_SIZE(HDRP(bp));
+    size_t index = get_index(size);
+    char *start = seg_freelist_head + index * DSIZE;
+    char *prev = GET_PRED(bp);
+    char *next = GET_SUCC(bp);
+
+    /* The list is empty */
+    if (start == NULL) return;
+
+    /* The node to be deleted is the first node in free list */
+    else if (prev == NULL) {
+        PUT_LONG(start, next);
+        if (next != NULL) PUT_LONG(PRED(next), NULL);
+    }
+
+    /* The node in the middle of free list */
+    else {
+        PUT_LONG(SUCC(prev), next);
+        if (next != NULL) PUT_LONG(PRED(next), prev);
+    }
+}
+
 /* 
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
 {
+    /* Create the free list part */
+    if ((seg_freelist_head = mem_sbrk(FREELIST_NUM * DSIZE)) == (void *) -1)
+        return -1;
+    for (int i = 0; i < FREELIST_NUM; ++i)
+        PUT_LONG(seg_freelist_head + i * DSIZE, NULL);
+    
+    /* Create the initial empty heap */
+    if ((heap_listp = mem_sbrk(4 * WSIZE)) == (void *) -1)
+        return -1;
+    PUT(heap_listp, 0);
+    PUT(heap_listp + (1 * WSIZE), PACK(DSIZE, 1));
+    PUT(heap_listp + (2 * WSIZE), PACK(DSIZE, 1));
+    PUT(heap_listp + (3 * WSIZE), PACK(0 ,1));
+    heap_listp += (2 * WSIZE);
+
+    /* Extend the empty heap with a free block of CHUNKSIZE bytes */
+    if (extend_heap(CHUNKSIZE/WSIZE) == NULL)
+        return -1;
+    #ifdef DEBUG
+        printf("Function: mm_init\n");
+        print_freeList();
+        print_heap();
+    #endif
     return 0;
+
 }
 
 /* 
@@ -41,14 +195,46 @@ int mm_init(void)
  */
 void *mm_malloc(size_t size)
 {
-    int newsize = ALIGN(size + SIZE_T_SIZE);
-    void *p = mem_sbrk(newsize);
-    if (p == (void *)-1)
-	return NULL;
-    else {
-        *(size_t *)p = size;
-        return (void *)((char *)p + SIZE_T_SIZE);
+    size_t asize;           /* Adjusted block size */
+    size_t extendsize;      /* Amount to extend heap if no fit */
+    char *bp;
+
+    /* Ignore spurious requests */
+    if (size == 0)
+        return NULL;
+
+    if (size == 112) size = 128;
+    if (size == 448) size = 512;
+
+    /* Adjust block size to include overhead and alignment reqs */
+    if (size <= 2 * DSIZE)
+        asize = 3 * DSIZE;
+    else
+        asize = DSIZE * ((size + (DSIZE) + (DSIZE - 1)) / DSIZE);
+
+     
+    /* Search the free list for a fit */
+    if ((bp = find_fit(asize)) != NULL) {
+        place(bp, asize);
+        #ifdef DEBUG
+            printf("Function: mm_malloc size: %d\n", asize);
+            print_freeList();
+            print_heap();
+        #endif
+        return bp;
     }
+
+    /* No fit found. Get more memory and place the block */
+    extendsize = MAX(asize, CHUNKSIZE);
+    if ((bp = extend_heap(extendsize / WSIZE)) == NULL)
+        return NULL;
+    place(bp, asize);
+    #ifdef DEBUG
+        printf("Function: mm_malloc size: %d\n", asize);
+        print_freeList();
+        print_heap();
+    #endif
+    return bp;
 }
 
 /*
@@ -56,6 +242,16 @@ void *mm_malloc(size_t size)
  */
 void mm_free(void *ptr)
 {
+    size_t size = GET_SIZE(HDRP(ptr));
+
+    PUT(HDRP(ptr), PACK(size, 0));
+    PUT(FTRP(ptr), PACK(size, 0));
+    coalesce(ptr);
+    #ifdef DEBUG
+        printf("Function: mm_free size: %llu\n", size);
+        print_freeList();
+        print_heap();
+    #endif
 }
 
 /*
@@ -63,31 +259,252 @@ void mm_free(void *ptr)
  */
 void *mm_realloc(void *ptr, size_t size)
 {
+
+    if (ptr == NULL) 
+        return mm_malloc(size);
+    else if (size == 0) {
+        mm_free(ptr);
+        return NULL;
+    }
+
+    /* Initialize some variables */
     void *oldptr = ptr;
     void *newptr;
-    size_t copySize;
-    
-    newptr = mm_malloc(size);
-    if (newptr == NULL)
-      return NULL;
-    copySize = *(size_t *)((char *)oldptr - SIZE_T_SIZE);
-    if (size < copySize)
-      copySize = size;
-    memcpy(newptr, oldptr, copySize);
-    mm_free(oldptr);
-    return newptr;
+    size_t asize;
+
+    /* Adjust block size to include overhead and alignment reqs */
+    if (size <= 2 * DSIZE) asize = 3 * DSIZE;
+    else asize = DSIZE * ((size + (DSIZE) + (DSIZE - 1)) / DSIZE);
+
+    /* If asize <= block size, don't change it */
+    if (asize <= GET_SIZE(HDRP(ptr))) {
+        return ptr;
+    }
+    /* Otherwise */
+    else {
+        size_t prev_alloc = GET_ALLOC(HDRP(PREV_BLKP(oldptr)));
+        size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(oldptr)));
+        size_t cur_size = GET_SIZE(HDRP(oldptr));
+        size_t extendsize;
+
+        if (prev_alloc && !next_alloc) {
+            cur_size += GET_SIZE(HDRP(NEXT_BLKP(oldptr)));
+            if (cur_size >= asize) {
+                remove_node(NEXT_BLKP(oldptr));
+                PUT(HDRP(oldptr), PACK(cur_size, 1));
+                PUT(FTRP(oldptr), PACK(cur_size, 1));
+                #ifdef DEBUG
+                    printf("Function: mm_realloc size: %llu\n", asize);
+                    print_freeList();
+                    print_heap();
+                #endif
+                return oldptr;
+            }
+        }
+        else if (!prev_alloc && next_alloc) {
+            cur_size += GET_SIZE(HDRP(PREV_BLKP(oldptr)));
+            if (cur_size >= asize) {
+                newptr = PREV_BLKP(oldptr);
+                remove_node(newptr);
+                PUT(HDRP(newptr), PACK(cur_size, 1));
+                PUT(FTRP(newptr), PACK(cur_size, 1));
+                memcpy(newptr, oldptr, GET_SIZE(HDRP(oldptr)) - DSIZE);
+                #ifdef DEBUG
+                    printf("Function: mm_realloc size: %llu\n", asize);
+                    print_freeList();
+                    print_heap();
+                #endif
+                return newptr;
+            }
+        }
+        else if (!prev_alloc && !next_alloc) {
+            cur_size += GET_SIZE(HDRP(PREV_BLKP(oldptr))) + GET_SIZE(HDRP(NEXT_BLKP(oldptr)));
+            if (cur_size >= asize) {
+                newptr = PREV_BLKP(oldptr);
+                remove_node(PREV_BLKP(oldptr));
+                remove_node(NEXT_BLKP(oldptr));
+                PUT(HDRP(newptr), PACK(cur_size, 1));
+                PUT(FTRP(newptr), PACK(cur_size, 1));
+                memcpy(newptr, oldptr, GET_SIZE(HDRP(oldptr)) - DSIZE);
+                #ifdef DEBUG
+                    printf("Function: mm_realloc size: %llu\n", asize);
+                    print_freeList();
+                    print_heap();
+                #endif
+                return newptr;
+            }
+        }
+
+        newptr = mm_malloc(asize);
+        memcpy(newptr, oldptr, GET_SIZE(HDRP(oldptr)) - DSIZE);
+        mm_free(oldptr);
+        #ifdef DEBUG
+            printf("Function: mm_realloc size: %llu\n", asize);
+            print_freeList();
+            print_heap();
+        #endif
+        return newptr;
+
+    }
+
 }
 
+/**
+ * @brief extend the heap by CHUNKSIZE
+ * @param words the number of words
+ * @return void* start of the new block
+ */
+static void *extend_heap(size_t words)
+{
+    char *bp;
+    size_t size;
 
+    /* Allocate an even number of words to maintain alignment */
+    size  = (words % 2) ? (words + 1) * WSIZE : words * WSIZE;
+    if ((long) (bp = mem_sbrk(size)) == -1)
+        return NULL;
 
+    /* Initialize free block header/footer and the epilogue header */
+    PUT(HDRP(bp), PACK(size, 0));
+    PUT(FTRP(bp), PACK(size, 0));
+    PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1));
 
+    /* Coalesce if the previous block was free */
+    return coalesce(bp);
+}
 
+/**
+ * @brief coalesce blocks around the block(bp)
+ * @param bp the block to be coalesced
+ * @return void* the start of new block
+ */
+static void *coalesce(void *bp)
+{
+    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+    size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+    size_t size = GET_SIZE(HDRP(bp));
 
+    if (prev_alloc && next_alloc) {         /* Case 1 */
+        insert_node(bp);
+        return bp;
+    }
 
+    else if (prev_alloc && !next_alloc) {   /* Case 2 */
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+        remove_node(NEXT_BLKP(bp));
+        PUT(HDRP(bp), PACK(size, 0));
+        PUT(FTRP(bp), PACK(size, 0));
+        insert_node(bp);
+    }
 
+    else if (!prev_alloc && next_alloc) {   /* Case 3 */
+        size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+        remove_node(PREV_BLKP(bp));
+        PUT(FTRP(bp), PACK(size, 0));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+        insert_node(bp);
+    }
 
+    else {                                  /* Case 4 */
+        size += GET_SIZE(HDRP(PREV_BLKP(bp))) +
+            GET_SIZE(FTRP(NEXT_BLKP(bp)));
+        remove_node(PREV_BLKP(bp));
+        remove_node(NEXT_BLKP(bp));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+        insert_node(bp);
+    }
+    return bp;
+}
 
+/**
+ * @brief find a suitable block for block having size of asize
+ * @param asize the size of block for which we search
+ * @return void* the address of the payload of the block we search for
+ */
+static void *find_fit(size_t asize)
+{
+    /* Search free-list */
+    size_t index = get_index(asize);
+    while (index < FREELIST_NUM) {
+        char *start = *(unsigned long *) (seg_freelist_head + index * DSIZE);
+        while (start != NULL && asize > GET_SIZE(HDRP(start)))
+            start = GET_SUCC(start);
+        if (start == NULL) ++index; /* No found in this level */
+        else return start;          /* Found */
+    }
+    return NULL;    /* No fit */
+}
 
+/**
+ * @brief malloc a block in the given address
+ * @param bp the given address to malloc a block
+ * @param asize the size of the block to be allocated
+ */
+static void place(void *bp, size_t asize)
+{
+    size_t csize = GET_SIZE(HDRP(bp));
+    
+    remove_node(bp);
+    if ((csize - asize) >= (3 * DSIZE)) {
+        PUT(HDRP(bp), PACK(asize, 1));
+        PUT(FTRP(bp), PACK(asize, 1));
+        bp = NEXT_BLKP(bp);
+        PUT(HDRP(bp), PACK(csize - asize, 0));
+        PUT(FTRP(bp), PACK(csize - asize, 0));
+        insert_node(bp);
+    }
+    else {
+        PUT(HDRP(bp), PACK(csize, 1));
+        PUT(FTRP(bp), PACK(csize, 1));
+    }
+}
 
+/**
+ * @brief Checker for segregated free list header
+ */
+void seg_header_checker()
+{
+    printf("Checker: seg_header_checker\n");
+    for (int i = 0; i < FREELIST_NUM; ++i) {
+        char *head = * (unsigned long *) (seg_freelist_head + i * DSIZE);
+        printf("Free list %d: %llu\n", i, head);
+    }
+    printf("\n");
+}
 
+/**
+ * @brief Print segregated free list 
+ */
+void print_freeList()
+{
+    printf("Checker: print_freeList\n");
+    for (int i = 0; i < FREELIST_NUM; ++i) {
+        char *head = * (unsigned long *) (seg_freelist_head + i * DSIZE);
+        printf("Free list %d: %llu\n", i, head);
+        while (head != NULL) {
+            printf("Address: %llu size: %d ->", head, GET_SIZE(HDRP(head)));
+            head = GET_SUCC(head);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
 
+/**
+ * @brief print the block in heap
+ */
+void print_heap()
+{
+    void *bp;
+    
+    printf("Checker: print_heap\n");
+    for (bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)) {
+        size_t size = GET_SIZE(HDRP(bp));
+        char alloc = GET_ALLOC(HDRP(bp)) ? 'a' : 'f';
+        printf("size: %d %c ->", size, alloc);
+    }
+    printf("\n\n");
+}
--- lab9/proxy.c
+++ lab9/proxy.c
@@ -6,14 +6,60 @@
 
 #include "csapp.h"
 #include <stdarg.h>
+#include <string.h>
 #include <sys/select.h>
+#include <pthread.h>
 
 /*
  * Function prototypes
  */
 int parse_uri(char *uri, char *target_addr, char *path, char *port);
+
 void format_log_entry(char *logstring, struct sockaddr_in *sockaddr, char *uri, size_t size);
 
+void doit(int fd, SA *sa);
+
+void *doit_one_thread(void *vargp);
+
+ssize_t Rio_readnb_w(rio_t *rp, void *usrbuf, size_t n)
+{
+    ssize_t size;
+    
+    if ((size = rio_readnb(rp, usrbuf, n)) < 0) {
+        fprintf(stderr, "An error occurs in Rio_readnb_w!\n");
+        return -1;
+    }
+
+    return size;
+}
+
+ssize_t Rio_readlineb_w(rio_t *rp, void *usrbuf, size_t maxlen)
+{
+    ssize_t size;
+
+    if ((size = rio_readlineb(rp, usrbuf, maxlen)) < 0) {
+        fprintf(stderr, "An error occurs in Rio_readlineb_w\n");
+        return -1;
+    }
+
+    return size;
+}
+
+ssize_t Rio_writen_w(int fd, void *usrbuf, size_t n)
+{
+    ssize_t size;
+
+    if ((size = rio_writen(fd, usrbuf, n)) < 0) {
+        fprintf(stderr, "An error occurs in Rio_writen_w\n");
+        return -1;
+    }
+    return size;
+}
+
+typedef struct info {
+    int *connfd;
+    SA *clientaddr;
+} info_t;
 
 /*
  * main - Main routine for the proxy program
@@ -26,7 +72,145 @@ int main(int argc, char **argv)
         exit(0);
     }
 
+    int listenfd;
+    int *connfd;
+    socklen_t clientlen;
+    struct sockaddr_storage *clientaddr;
+    char client_hostname[MAXLINE], client_port[MAXLINE];
+    pthread_t tid;
+    info_t *info;
+
+    listenfd = Open_listenfd(argv[1]);
+    while (1) {
+        clientlen = sizeof(clientaddr);
+        clientaddr = Malloc(sizeof(struct sockaddr_storage));
+        connfd = Malloc(sizeof(int));
+        *connfd = Accept(listenfd, (SA *) clientaddr, &clientlen);
+        info_t *info = Malloc(sizeof(info_t));
+        info->connfd = connfd;
+        info->clientaddr = clientaddr;
+        Pthread_create(&tid, NULL, doit_one_thread, info);
+        // Getnameinfo((SA *) &clientaddr, clientlen, client_hostname, MAXLINE, client_port, MAXLINE, 0);
+        // printf("Connected to (%s %s)\n", client_hostname, client_port);
+        // doit(*connfd, (SA *) clientaddr);
+        // Close(connfd);
+    }
+
     exit(0);
+}
+
+void *doit_one_thread(void *vargp)
+{
+    info_t *info = (info_t *) vargp;
+    int connfd = *info->connfd;
+    SA clientaddr = *info->clientaddr;
+    Pthread_detach(pthread_self());
+    Free(vargp);
+    doit(connfd, (SA *) &clientaddr);
+    Close(connfd);
+    return NULL;
+
+}
+
+void doit(int fd, SA *sa) 
+{
+    int serverfd;           // The fd connected to server
+    ssize_t nread;          // Buf size read from server
+    ssize_t totalLength = 0;    // Content length of response from server
+    ssize_t requestBodyLen;     // The length of request body (if exists)
+    ssize_t responseBodyLen;
+    char method[MAXLINE], uri[MAXLINE], version[MAXLINE];
+    char hostname[MAXLINE], pathname[MAXLINE], port[MAXLINE];
+    char req_from_client[MAXLINE] = {'\0'}, req_to_server[MAXLINE] = {'\0'}; 
+    char respon_from_server[MAXLINE] = {'\0'}, respon_to_client[MAXLINE] = {'\0'};
+    char longstring[MAXLINE];
+    rio_t rio, server_rio;
+
+    /* Read request line from the client */
+    Rio_readinitb(&rio, fd);
+    if (Rio_readlineb_w(&rio, req_from_client, MAXLINE) <= 0) {
+        fprintf(stderr, "read error: invalid request line from client\n");
+        return;
+    }
+    /* Get method, uri, version */
+    if (sscanf(req_from_client, "%s %s %s", method, uri, version) != 3) {
+        fprintf(stderr, "sscanf error: arguments number is invalid\n");
+        return;
+    } 
+    /* Get hostname, pathname and port from uri */
+    if (parse_uri(uri, hostname, pathname, port) == -1) {
+        fprintf(stderr, "parse error: invalid uri");
+        return;
+    }      
+
+    /* Parse request line and send it to server */
+    serverfd = Open_clientfd(hostname, port);
+    sprintf(req_to_server, "%s /%s %s\r\n", method, pathname, version); // Make up request line for server
+    Rio_writen_w(serverfd, req_to_server, strlen(req_to_server));
+    memset(req_to_server, 0, MAXLINE);
+
+    /* Read request header from the client and send it to server */
+    while (strcasecmp(req_from_client, "\r\n") != 0 
+        && (nread = Rio_readlineb_w(&rio, req_from_client, MAXLINE)) != 0) {
+        if (strncasecmp(req_from_client, "Content-Length", 14) == 0) {
+            requestBodyLen = strtol(req_from_client + 16, NULL, 10);
+        }
+        sprintf(req_to_server, "%s%s", req_to_server, req_from_client);
+        // Rio_writen_w(serverfd, req_from_client, MAXLINE);
+    }
+    if (nread == 0) {
+        fprintf(stderr, "Wrong request header from client\n");
+        close(serverfd);
+        return;
+    }
+    Rio_writen_w(serverfd, req_to_server, strlen(req_to_server));
+    memset(req_from_client, 0, MAXLINE);
+
+    /* If there exists request body */
+    if (strcmp(method, "GET")) {
+        for (int i = 0; i < requestBodyLen; ++i) {
+            if (Rio_readnb_w(&rio, req_from_client, 1) == 0) break;
+            Rio_writen_w(serverfd, req_from_client, 1);
+        }
+    }
+
+    /* Read response header */
+    Rio_readinitb(&server_rio, serverfd);
+    while (strcasecmp(respon_from_server, "\r\n") != 0 
+        && (nread = Rio_readlineb_w(&server_rio, respon_from_server, MAXLINE)) != 0){
+        if (strncasecmp(respon_from_server, "Content-Length", 14) == 0) {
+            responseBodyLen = strtol(respon_from_server + 16, NULL, 10);
+            totalLength += responseBodyLen;
+        }
+        totalLength += strlen(respon_from_server);
+        sprintf(respon_to_client, "%s%s", respon_to_client, respon_from_server);
+        // Rio_writen_w(fd, respon_from_server, MAXLINE);
+    }
+    if (nread == 0) {
+        fprintf(stderr, "Wrong response header from server\n");
+        close(serverfd);
+        return;
+    }
+    Rio_writen_w(fd, respon_to_client, strlen(respon_to_client));
+    memset(respon_to_client, 0 , MAXLINE);
+
+    format_log_entry(longstring, sa, uri, totalLength);
+    printf("%s\n", longstring);
+
+    /* Read response body and send it to client */
+    while (Rio_readnb_w(&server_rio, respon_to_client, 1) > 0) {
+        Rio_writen_w(fd, respon_to_client, 1);
+        responseBodyLen--;
+    }
+    if (responseBodyLen > 0) {
+        fprintf(stderr, "Wrong response body from server\n");
+        close(serverfd);
+        return;
+    }
+
+    Close(serverfd);
+    return;
+    
 }
 
 
